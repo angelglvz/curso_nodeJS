4ec3349740665accbb8361d92f4d7bf1271a5760
angelglvz <angelglvz99@gmail.com>
2026-02-19 10:01:37 +0100

    pagina básica

--- Backend/2o_proyecto/1-http.js
+++ Backend/2o_proyecto/1-http.js
@@ -1,20 +1,28 @@
-const http = require('node:http') // protocolo HTTP
-// const { findAvailablePort } = require('./free-port.js')
-
-// console.log(process.env)
+const http = require('node:http'); // protocolo HTTP
+const fs = require('node:fs');
 
 const desiredPort = process.env.PORT ?? 3000
 
-const server = http.createServer((req, res) => {
-  console.log('request received')
-  res.end('Hola mundo')
+const processRequest = ((req, res) => {
+
+    if(req.url === '/'){
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html; charset=utf8')
+        res.end('<h1>Bienvenido a mi página</h1>')
+    }
+    else if(req.url === '/contacto'){
+        res.statusCode = 200
+        res.setHeader('Content-Type', 'text/html; charset=utf8')
+        res.end('<h1>llamame</h1>')
+    }
+    else{
+        res.statusCode = 404
+        res.setHeader('Content-Type', 'text/html; charset=utf8')
+        res.end('<h1>404 not found</h1>')
+    }
 })
 
-// findAvailablePort(desiredPort).then(port => {
-//   server.listen(port, () => {
-//     console.log(`server listening on port http://localhost:${port}`)
-//   })
-// })
+const server = http.createServer(processRequest)
 
 server.listen(desiredPort, () =>{
     console.log(`server listening to http://localhost:${desiredPort}`)

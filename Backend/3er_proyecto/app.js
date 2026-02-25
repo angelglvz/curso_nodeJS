5d827a3c36a21f6669e9a0e9482dbe2b73c33cd2
angelglvz <angelglvz99@gmail.com>
2026-02-25 18:47:57 +0100

    post method

--- Backend/3er_proyecto/app.js
+++ Backend/3er_proyecto/app.js
@@ -1,17 +1,51 @@
 const express = require('express')
 const movies = require('./movies.json')
+const crypto = require('node:crypto') // te permite crear id unicas
 
 const app = express()
 
 app.disable('x-powered-by') // quita la cabecera
+app.use(express.json) // un middleware que ayuda a la conversion de json con express
 
 app.get('/', (req, res) => {
   res.json({ message: 'hola mundo' })
 })
 
 app.get('/movies', (req, res) => {
+  const { genre } = req.query
+  if (genre) {
+    const filteredMovies = movies.filter(
+      movie => movie.genre.some(g => g.toLowerCase() === genre.toLocaleLowerCase())
+    )
+    return res.json(filteredMovies)
+  }
   res.json(movies)
 })
+
+app.get('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+  res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+  const newMovie = {
+    id: crypto.randomUUID(),
+    title,
+    genre,
+    year,
+    director,
+    duration,
+    rate: rate ?? 0,
+    poster
+  }
+
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
+})
+
 const PORT = process.env.PORT ?? 1234
 
 app.listen(PORT, () => {

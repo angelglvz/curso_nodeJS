8f8b247896aa66aadb1f1c714213658e8ac35533
angelglvz <angelglvz99@gmail.com>
2026-02-26 11:08:05 +0100

    creados metodos post, patch y delete, usado libreria zod

--- Backend/3er_proyecto/app.js
+++ Backend/3er_proyecto/app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const movies = require('./movies.json')
 const crypto = require('node:crypto') // te permite crear id unicas
+const { validateMovie, validatePartialMovie } = require('./movies')
 
 const app = express()
 
 app.disable('x-powered-by') // quita la cabecera
-app.use(express.json) // un middleware que ayuda a la conversion de json con express
+app.use(express.json()) // un middleware que ayuda a la conversion de json con express
 
 app.get('/', (req, res) => {
   res.json({ message: 'hola mundo' })
@@ -30,20 +31,57 @@ app.get('/movies/:id', (req, res) => {
 })
 
 app.post('/movies', (req, res) => {
+  const result = validateMovie(req.body)
+
+  if (result.error) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
   const newMovie = {
     id: crypto.randomUUID(),
-    title,
-    genre,
-    year,
-    director,
-    duration,
-    rate: rate ?? 0,
-    poster
+    ...result.data
   }
 
   movies.push(newMovie)
 
   res.status(201).json(newMovie)
+})
+
+app.patch('/movies/:id', (req, res) => {
+  const result = validatePartialMovie(req.body)
+
+  if (!result.success) {
+    return res.status(400).json({ error: JSON.parse(result.error.message) })
+  }
+
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  const updateMovie = {
+    ...movies[movieIndex],
+    ...result.data
+  }
+
+  movies[movieIndex] = updateMovie
+
+  return res.json(updateMovie)
+})
+
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+
+  movies.splice(movieIndex, 1)
+
+  return res.json({ message: 'Movie deleted' })
 })
 
 const PORT = process.env.PORT ?? 1234
---
+++ Backend/3er_proyecto/movies.js
@@ -0,0 +1,33 @@
+const z = require('zod')
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: 'Movie title must be a string',
+    required_error: 'Movie title is required.'
+  }),
+  year: z.number().int().positive().min(1900).max(2080),
+  director: z.string(),
+  duration: z.number().int().positive(),
+  rate: z.number().min(0).max(10),
+  poster: z.string(),
+  genre: z.array(
+    z.enum(['Action', 'Adventure', 'Comedy', 'Crime', 'Drama', 'Fantasy', 'Horror', 'Thriller', 'Sci-Fi']),
+    {
+      required_error: 'Movie genre is required.',
+      invalid_type_error: 'Movie genre must be an array of enun Genre'
+    }
+  )
+})
+
+function validateMovie (object) {
+  return movieSchema.safeParse(object)
+}
+
+function validatePartialMovie (input) {
+  return movieSchema.partial().safeParse(input)
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie
+}
